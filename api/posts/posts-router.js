4605196855a079d3d3c24b4e4278b6a02b2f24ea
Spencerp34 <spencerp34@yahoo.com>
2021-08-03 15:03:47 -0600

    halfway done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,64 @@
 // implement your posts router here
+ const Post = require('./posts-model');
+ const express = require('express');
+ const router = express.Router();
+
+ router.get('/', async(req, res) => {
+     try{
+         const post = await Post.find();
+         res.status(200).json(post)
+     }catch(err){
+        res.status(500).json({ message: "The posts information could not be retrieved" })
+     }
+ })
+
+ router.get('/:id', async(req, res) => {
+    try{
+        const id = req.params.id
+        const post = await Post.findById(id);
+        if(!post){
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }else{
+            res.status(200).json(post)
+        }
+    }catch{
+        res.status(500).json({ message: "The post information could not be retrieved" })
+    }
+ })
+
+ router.post('/', async(req, res) => {
+     try{
+         const {title, contents} = req.body
+         if( !title || !contents ){
+            res.status(400).json({ message: "Please provide title and contents for the post" })
+        }else{
+            const {id} = await Post.insert(req.body)
+            const newPost = await Post.findById(id)
+             res.status(201).json(newPost)
+        }
+     }catch(err){
+        res.status(500).json({ message: "There was an error while saving the post to the database" })
+     }
+ })
+
+ router.put('/:id', async(req, res) => {
+     try{
+        const {id, post} = req.params
+        const updated = Post.update(id, post)
+        res.status(200).json(updated)
+     }catch(err){
+        res.status(500).json({ message: "The post information could not be modified" })
+     }
+ })
+
+ router.delete('/:id', async(req, res) => {
+     try{
+        const id = req.params.id;
+        const deleted = Post.remove(id)
+        res.status(200).json(deleted)
+     }catch(err){
+        res.status(500).json({ message: "The post could not be removed" })
+     }
+ })
+
+ module.exports = router
